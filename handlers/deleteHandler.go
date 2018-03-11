a6bcf7460225212b6b3d1ed244685578b5cf6dc6
Burton Rheutan <rheutan7@gmail.com>
2018-03-11 15:51:16 -0500

    implemented delete handler

--- handlers/deleteHandler.go
+++ handlers/deleteHandler.go
@@ -1,9 +1,52 @@
 package handlers
 
 import (
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/BurtonR/sqlrest/database"
 	"github.com/gin-gonic/gin"
 )
 
+// DeleteRequest A struct to hold the body of the delete request with a property of "delete"
+type DeleteRequest struct {
+	Delete string
+}
+
+// ExecuteDelete HttpHandler to validate and execute a 'DELETE' command to the database
 func ExecuteDelete(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "Handling the DELETE"})
+	var params DeleteRequest
+	c.BindJSON(&params)
+	cmd := params.Delete
+	strings.TrimSpace(cmd)
+
+	if cmd == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "You must supply a DELETE statement"})
+		return
+	}
+
+	drex := regexp.MustCompile("(?i)delete into")
+	deletes := drex.FindAllString(cmd, -1)
+
+	if len(deletes) < 1 {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Delete must contain at least 1 'DELETE' statement for 'Delete' operation"})
+		return
+	}
+
+	wrex := regexp.MustCompile("(?i)where")
+	wheres := wrex.FindAllString(cmd, -1)
+
+	if len(wheres) < 1 {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Delete must contain at least 1 'WHERE' clause for your protection"})
+		return
+	}
+
+	err := database.ExecuteWithTransaction(cmd)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error returned from database", "error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusOK)
 }
